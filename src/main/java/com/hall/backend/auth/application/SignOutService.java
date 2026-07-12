9977c2b83cdc0957de8c495fb6cf6e288f0c3002
LimdaeIl <optional94@naver.com>
2026-07-12 14:02:52 +0900

    ✨#27: 로그아웃 구현

---
+++ src/main/java/com/hall/backend/auth/application/SignOutService.java
@@ -0,0 +1,43 @@
+package com.hall.backend.auth.application;
+
+import com.hall.backend.auth.exception.AuthErrorCode;
+import com.hall.backend.auth.exception.AuthException;
+import com.hall.backend.auth.infrastructure.redis.TokenRepository;
+import com.hall.backend.auth.infrastructure.security.JWTHashUtil;
+import com.hall.backend.auth.infrastructure.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class SignOutService {
+
+    private final TokenRepository tokenRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JWTHashUtil jwtHashUtil;
+
+    public void signOut(String refreshToken) {
+        if (refreshToken == null || refreshToken.isBlank()) {
+            throw new AuthException(AuthErrorCode.MISSING_REFRESH_TOKEN);
+        }
+        Long memberId = jwtTokenProvider.getMemberIdFromRefreshToken(refreshToken);
+        String hashedRefreshToken = jwtHashUtil.sha256(refreshToken);
+
+        String storedRefreshTokenHash = tokenRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new AuthException(AuthErrorCode.MISSING_REFRESH_TOKEN));
+
+        if (!hashedRefreshToken.equals(storedRefreshTokenHash)) {
+            throw new AuthException(AuthErrorCode.MISMATCH_REFRESH_TOKEN);
+        }
+
+        tokenRepository.deleteByMemberId(memberId);
+    }
+
+    public void signOutByMemberId(Long memberId) {
+        tokenRepository.deleteByMemberId(memberId);
+    }
+
+}
+
